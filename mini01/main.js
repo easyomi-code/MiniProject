0dd31fbca25fb8ec088bf7d4a2eb1c51c94e3b25
easyomi-code <easyomi.code@gmail.com>
2024-07-15 13:07:06 +0900

    main.js

--- mini01/main.js
+++ mini01/main.js
@@ -3,23 +3,40 @@ let stringArr = ['Learn to HTML', 'Learn to CSS', 'Learn to JavaScript'];
 let selectString = stringArr[Math.floor(Math.random() * stringArr.length)];
 let selectStringArr = selectString.split('');
 
-// randomArr: 매개변수(Parameter);
+function randomString() {
+  let stringArr = ['Learn to HTML', 'Learn to CSS', 'Learn to JavaScript'];
+  let selectString = stringArr[Math.floor(Math.random() * stringArr.length)];
+  let selectStringArr = selectString.split('');
+
+  return selectStringArr;
+}
+
+// 타이핑 리셋
+function resetTyping() {
+  target.textContent = '';
+  dynamic(randomString());
+}
+
+// ◎ 한 글자씩 텍스트 출력하는 함수
+// randomArr: 매개변수(Parameter)
 function dynamic(randomArr) {
-  console.log(randomArr);
+  // console.log(randomArr);
+  // if문: 배열에 문자열이 남아 있다면
   if (randomArr.length > 0) {
     // shift(): 배열의 앞에서부터 출력
     target.textContent += randomArr.shift();
     setTimeout(function () {
       dynamic(randomArr);
     }, 80);
+  } else {
+    setTimeout(resetTyping, 3000);
   }
 }
 
-// selectStringArr: 인자(Argument)
-dynamic(selectStringArr);
+dynamic(randomString());
 
-console.log(selectString);
-console.log(selectStringArr);
+// console.log(selectString);
+// console.log(selectStringArr);
 
 // 커서 깜빡임 효과
 function blink() {
